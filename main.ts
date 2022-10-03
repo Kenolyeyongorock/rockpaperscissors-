827fcb6daa927c7721dc912a3fe6223fbfd901e6
Kenolyeyongorock <114928549+Kenolyeyongorock@users.noreply.github.com>
2022-10-03 09:53:28 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,29 @@
+input.onButtonPressed(Button.A, function () {
+    hand += 1
+    if (hand > 3) {
+        hand = 1
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # # # .
+            . # # # .
+            . . . . .
+            `)
+    } else if (0 == 0) {
+    	
+    } else {
+    	
+    }
+})
+let hand = 0
+hand = 1
+basic.showLeds(`
+    . . . . .
+    . # # # .
+    . # # # .
+    . # # # .
+    . . . . .
+    `)
+basic.forever(function () {
+	
+})
